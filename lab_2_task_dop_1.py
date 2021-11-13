6a39ecbb22ebe9005b324c4fc4a000f510c11f5e
Legorov228 <92169579+Legorov228@users.noreply.github.com>
2021-11-13 16:44:22 +0000

    lol

--- lab_2_task_dop_1.py
+++ lab_2_task_dop_1.py
@@ -1,10 +1,15 @@
-a = int(input())
-b = int(input())
-c = int(input())
+a = int(input('Введите первый коэффициент: '))
+b = int(input('Введите второй коэффициент: '))
+c = int(input('Введите третий коэффициент: '))
 
-if :
-  print('Два корня')
-elif  :
-  print('Один корень')
-else:
-  print('Нет корней')
+if D > 0:
+  f = b ** 2 - 4 * a * c
+  x1 = (-b + math.sqrt(f)) / (2 * a)
+  x2 = (-b - math.sqrt(f)) / (2 * a)
+  print(f'Первый корень: {x1}')
+  print(f'Второй корень: {x2}')
+  if D = 0:
+    x = -b / (2 * a)
+    print(f'Один корень: {x}')
+  else:
+    print('Нет корней!')
