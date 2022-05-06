d160c1cbf37be0183d82edf8267efeb8997fca10
GodGame748 <92169579+Legorov228@users.noreply.github.com>
2022-05-06 14:20:24 +0000

    пао

--- lec_15_kerf.py
+++ lec_15_kerf.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import mpl_toollkits.mplot3d.axes3d as plt3d
+import mpl_toolkits.mplot3d.axes3d as plt3d
 
 fig = plt.figure()
 ax = plt3d.Axes3D(fig)
